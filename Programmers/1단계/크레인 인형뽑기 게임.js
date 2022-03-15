a08d6817c149b232e4eda9872896994096191496
minjeong-dev <100185951+minjeong-dev@users.noreply.github.com>
2022-03-16 01:22:28 +0900

    Update 크레인 인형뽑기 게임.js

--- Programmers/1단계/크레인 인형뽑기 게임.js
+++ Programmers/1단계/크레인 인형뽑기 게임.js
@@ -1,4 +1,4 @@
-//문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/64061
+/*문제 링크 : https://programmers.co.kr/learn/courses/30/lessons/64061*/
 
 function solution(board, moves) {
     var answer = 0;
